48e7f3cf18c795832b3046fbae4db07520cc8870
Nadeem Siddique <nadeem@sprinto.com>
2025-06-08 19:11:49 +0530

    minor improvements

--- main.go
+++ main.go
@@ -17,12 +17,13 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "safe-domain-search",
+		Title:  "Safe Domain Search",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
+
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
